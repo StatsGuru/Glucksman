043e60154623769cf7787a24625fecbd0a6dec88
StatsGuru <jdw440@stern.nyu.edu>
2014-11-22 19:06:53 -0500

    Lipitor NDCs
    More to commit

--- Scripts/DURprocessing.py
+++ Scripts/DURprocessing.py
@@ -20,6 +20,39 @@ import matplotlib.pyplot as plt
 Inputing the data
 """
 
-file = 'https://raw.githubusercontent.com/StatsGuru/Glucksman/9a995d7f93f36a5fcde83ede8a660478a3711f71/NationalDUR2013.TXT'
+file = 'https://raw.githubusercontent.com/StatsGuru/Glucksman/2b600409d92339530eef30e7d42995a984625550/NationalDUR2013.TXT'
 df = pd.read_csv(file, sep='|')
 print(df.head())
+
+#%%
+
+df2 = list(df.columns.values)
+print(df2)
+rowtoappend = df[1:2]
+df3=pd.DataFrame(columns = df2)
+df3.append(rowtoappend)
+#%%
+
+df3=pd.DataFrame(columns = df2)
+df3.append(df[1])
+print(df3)
+
+df["ProductCode"]
+#%%
+dfproduct = pd.DataFrame(columns = list(df.columns.values))
+
+def productcodeexporter(code, df):
+    dfshort = pd.DataFrame(columns = list(df.columns.values))
+    for i in df.index:
+        if df["ProductCode"][i] == code:
+            rowtoappend = df[i:i+1]
+            dfshort = dfshort.append(rowtoappend)
+    return dfshort
+
+def multiproductexporter(rxlist, df):
+    dflong = pd.DataFrame(columns = list(df.columns.values))
+    for i in range(len(rxlist)):
+        dflong = dflong.append(productcodeexporter(rxlist[i], df))
+    return dflong
+    
+dfproduct = dfproduct.append(multiproductexporter([5810,5820], df))
